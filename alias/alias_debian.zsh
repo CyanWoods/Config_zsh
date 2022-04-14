f30bebd2cba3cb5afa37b66708b5e5ccef835b96
CyanWoods <i@cyanwoods.com>
2022-04-14 23:59:36 +0800

    .

--- alias/alias_debian.zsh
+++ alias/alias_debian.zsh
@@ -5,3 +5,5 @@ alias edzshenv="vim ~/.config/zsh/env/env_debian.zsh"
 alias smbstart="sudo mount /dev/sda2 ./raspi_smb/ && sudo systemctl start smbd"
 alias smbstop="sudo systemctl stop smbd && sudo umount ./raspi_smb"
 alias update="sudo apt update && sudo apt upgrade -y && sudo apt autoremove -y"
+
+alias rasp="/usr/bin/ssh  -T -f -N -g -R :25564:127.0.0.1:22 cyanwoods@cyanwoods.com"
