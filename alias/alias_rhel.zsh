71f6c2331dff819465011fec780a2f8148ca502b
CyanWoods <i@cyanwoods.com>
2022-04-15 08:58:32 +0800

    sharedisk on rhel VM

--- alias/alias_rhel.zsh
+++ alias/alias_rhel.zsh
@@ -1,2 +1,5 @@
 alias edzshali="vim ~/.config/zsh/alias/alias_rhel.zsh"
 alias edzshenv="vim ~/.config/zsh/env/env_rhel.zsh"
+
+
+alias sharedisk="/usr/bin/vmhgfs-fuse .host:/ $HOME/shared -o subtype=vmhgfs-fuse,allow_other"
