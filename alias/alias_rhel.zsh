1aadd130788a80fd58410b8092fac11910b8fe10
CyanWoods <i@cyanwoods.com>
2022-04-15 16:39:15 +0800

    .

--- alias/alias_rhel.zsh
+++ alias/alias_rhel.zsh
@@ -1,3 +1,4 @@
+/usr/bin/vmhgfs-fuse .host:/ $HOME/shared -o subtype=vmhgfs-fuse,allow_other
 alias edzshali="vim ~/.config/zsh/alias/alias_rhel.zsh"
 alias edzshenv="vim ~/.config/zsh/env/env_rhel.zsh"
 
