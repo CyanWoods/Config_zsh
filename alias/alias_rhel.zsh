d6a2273a977906e35f7bdd5a44d8c936d938ac54
CyanWoods <i@cyanwoods.com>
2022-04-18 17:47:44 +0800

    .

--- alias/alias_rhel.zsh
+++ alias/alias_rhel.zsh
@@ -2,3 +2,5 @@ alias edzshali="vim ~/.config/zsh/alias/alias_rhel.zsh"
 alias edzshenv="vim ~/.config/zsh/env/env_rhel.zsh"
 
 alias sharedisk="/usr/bin/vmhgfs-fuse .host:/ $HOME/shared -o subtype=vmhgfs-fuse,allow_other"
+
+alias update="sudo yum update -y && sudo yum autoremove -y"
