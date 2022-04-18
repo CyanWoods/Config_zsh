b5868f8346df6a627d99e6239a28244ac27e1b54
CyanWoods <i@cyanwoods.com>
2022-04-18 17:16:30 +0800

    .

--- alias/alias_rhel.zsh
+++ alias/alias_rhel.zsh
@@ -1,6 +1,4 @@
-/usr/bin/vmhgfs-fuse .host:/ $HOME/shared -o subtype=vmhgfs-fuse,allow_other
 alias edzshali="vim ~/.config/zsh/alias/alias_rhel.zsh"
 alias edzshenv="vim ~/.config/zsh/env/env_rhel.zsh"
-
 
 alias sharedisk="/usr/bin/vmhgfs-fuse .host:/ $HOME/shared -o subtype=vmhgfs-fuse,allow_other"
