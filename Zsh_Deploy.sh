b8c3d7c1eff7a8ea3712aa1e6af619761c262c0e
CyanWoods <i@cyanwoods.com>
2022-04-19 08:47:14 +0800

    .

--- Zsh_Deploy.sh
+++ Zsh_Deploy.sh
@@ -1,13 +1,14 @@
 cd $HOME 
 
+# install ohmyzsh
 if [ ! -d "$HOME/.oh-my-zsh/" ]; then
     sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 fi
 
+#check necessary plugins and themes
 if [ ! -d "$HOME/.oh-my-zsh/custom/themes/powerlevel10k" ]; then
     git clone --depth=1 https://gitee.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 fi
-
 
 if [ ! -d "$HOME/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting" ]; then
     git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
@@ -21,17 +22,16 @@ if [ ! -d "$HOME/.oh-my-zsh/custom/plugins/zsh-completions/zsh-autosuggestions" 
     git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 fi
 
+# replace exsist zshrc file
 if [  -f "$HOME/.zshrc" ]; then
-    mv $HOME/.zshrc $HOME/.zshrc.bak
+    mv $HOME/.zshrc $HOME/.zshrc.bak -y 
 fi
-
-
-
 
 if [ ! -d "$HOME/.config/zsh" ]; then
     git clone https://github.com/CyanWoods/zshconfig.git $HOME/.config/zsh
 elif [ -d "$HOME/.config/zsh" ]; then
     cd $HOME/.config/zsh && git pull -r 
+    cd $HOME
 fi
 
 ln -s $HOME/.config/zsh/zshrc $HOME/.zshrc 
