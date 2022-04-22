60aff616e808f4ba62199ecd5395f60d36c4d878
CyanWoods <i@cyanwoods.com>
2022-04-23 01:52:28 +0800

    .

--- alias/alias_debian.zsh
+++ alias/alias_debian.zsh
@@ -2,8 +2,8 @@ alias edzshali="vim ~/.config/zsh/alias/alias_debian.zsh"
 alias edzshenv="vim ~/.config/zsh/env/env_debian.zsh"
 
 
-alias smbstart="sudo mount /dev/sda2 ./raspi_smb/ && sudo systemctl start smbd"
-alias smbstop="sudo systemctl stop smbd && sudo umount ./raspi_smb"
+alias smbstart="sudo mount /dev/sda2 ./Raspi_SMB/ && sudo systemctl start smbd"
+alias smbstop="sudo systemctl stop smbd && sudo umount ./Raspi_SMB"
 alias update="sudo apt update && sudo apt upgrade -y && sudo apt autoremove -y"
 
 alias rasp="/usr/bin/ssh  -T -f -N -g -R :25564:127.0.0.1:22 cyanwoods@cyanwoods.com"
--- alias/alias_ubuntu.zsh
+++ alias/alias_ubuntu.zsh
@@ -4,3 +4,8 @@ alias edzshenv="vim ~/.config/zsh/env/env_ubuntu.zsh"
 
 alias update="sudo apt update && sudo apt upgrade -y && sudo apt autoremove -y"
 alias mc="/etc/init.d/minecraft"
+
+alias rasp="/usr/bin/ssh  -T -f -N -g -R :25564:127.0.0.1:22 cyanwoods@cyanwoods.com"
+
+alias smbstart="sudo mount /dev/sda2 ./Rasp_SMB/ && sudo systemctl start smbd"
+alias smbstop="sudo systemctl stop smbd && sudo umount ./Rasp_SMB"
