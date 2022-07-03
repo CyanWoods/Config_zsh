8192b6ecbad5b5b19228baeeed10bdd1f61681ca
CyanWoods <i@cyanwoods.com>
2022-07-03 14:32:37 +0800

    .

--- alias/alias_ubuntu.zsh
+++ alias/alias_ubuntu.zsh
@@ -4,6 +4,5 @@ alias edzshenv="vim ~/.config/zsh/env/env_ubuntu.zsh"
 alias smbstop="sudo systemctl stop smbd && sudo umount ./Raspi_SMB"
 alias update="sudo apt update && sudo apt upgrade -y && sudo apt autoremove -y"
 
-alias mc="/etc/init.d/minecraft"
-alias minecraft="/etc/init.d/minecraft"
+alias mc="~/mcadmin.sh"
 alias pro="export https_proxy=127.0.0.1:38457"
